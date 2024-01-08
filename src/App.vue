69fa3d20b22c616dbe83b5753db2bf934b8fde1f
aletardio <ale.tardio99@gmail.com>
2024-01-08 14:53:28 +0100

    create generals.scss and variables.scss

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,10 @@ export default {
 </script>
 <template lang="">
   <div>
-    
+    <h1>ciao</h1>
   </div>
 </template>
-<style lang="">
-  
+<style lang="scss">
+@use './styles/generals.scss';
+@use './styles/partials/variables' as *;
 </style>
